785b5850c0d93f91dbdc4672ff2489364c1f8ac0
Jocode <jocode@192.168.1.4>
2024-04-27 21:03:16 +0000

    first commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      This is a protected route.
+    </div>
+  );
+}
+
+export default Home;
